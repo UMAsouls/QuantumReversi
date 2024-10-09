1a9ed74ab5c97d3f448bc3d8adb4842725650499
UMAsouls <umaplay972@gmail.com>
2024-10-09 20:56:41 +0900

    [add] AIの設計図作成

---
+++ Assets/QuantumReversi/Interface/IAI.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Cysharp.Threading.Tasks;
+
+public interface IAI
+{
+
+    public UniTask SetStone();
+}
---
+++ Assets/QuantumReversi/Script/AI.cs
@@ -0,0 +1,31 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using Zenject;
+
+public class AI : IAI
+{
+    [Inject]
+    BoardGettableForAI board;
+
+    public async UniTask SetStone()
+    {
+        //おける場所
+        int[][] pos = board.JudgedPos;
+        //確率盤面
+        int[][] realBoard = board.RealBoard;
+
+        //置く位置(x, y)
+        int[] setPos = new int[2];
+        //置く石の確率
+        StoneType type = StoneType.TEN;
+
+
+        //こっからAIのコード
+
+        //
+
+       await board.SetStone(setPos[1], setPos[0], type);
+    }
+}
