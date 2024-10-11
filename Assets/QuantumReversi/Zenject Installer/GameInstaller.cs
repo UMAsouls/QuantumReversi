5f344aa1e75fa2e1712fc367ae212feb09ecffd1
UMAsouls <umaplay972@gmail.com>
2024-10-11 16:43:25 +0900

    [add] installerにheadMassの項目を追加

--- Assets/QuantumReversi/Zenject Installer/GameInstaller.cs
+++ Assets/QuantumReversi/Zenject Installer/GameInstaller.cs
@@ -3,11 +3,19 @@ using Zenject;
 
 public class GameInstaller : MonoInstaller
 {
+    [SerializeField]
+    private GameObject headMass;
+
     public override void InstallBindings()
     {
         Container
             .BindInterfacesTo<Stone>()
             .AsTransient();
 
+        Container
+            .Bind<HeadMass>()
+            .To<Mass>()
+            .FromComponentOn(headMass)
+            .AsTransient();
     }
 }
